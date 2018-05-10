0191fe62391ed9de876168a28a99c737f6eb4096
u6k <u6k.apps@gmail.com>
2018-05-11 05:20:32 +0900

    modify

--- rundeck-job/backup_investment-machine.sh
+++ rundeck-job/backup_investment-machine.sh
@@ -12,17 +12,3 @@ rm investment-machine.${BUILD_TIMESTAMP}.psql.dmp
 
 # upload to s3
 aws --endpoint-url ${AWS_S3_ENDPOINT} s3 sync . s3://${RD_OPTION_AWS_S3_BUCKET}/
-
-
-
-
-
-
-docker exec ${RD_OPTION_DB_CONTAINER_NAME} pg_dump -U ${RD_OPTION_DB_USER} ${RD_OPTION_DB_NAME} >redmine.${BUILD_TIMESTAMP}.psql.dmp
-7z a -t7z -mx=9 redmine.${BUILD_TIMESTAMP}.psql.dmp.7z redmine.${BUILD_TIMESTAMP}.psql.dmp
-rm redmine.${BUILD_TIMESTAMP}.psql.dmp
-
-docker cp ${RD_OPTION_APP_CONTAINER_NAME}:/usr/src/redmine/files/ attachment-file/
-
-# upload to s3
-aws --endpoint-url ${AWS_S3_ENDPOINT} s3 sync . s3://${RD_OPTION_AWS_S3_BUCKET}/
