d538be6cfbfadabe0ce1829983d83b09830b2255
u6k <u6k.apps@gmail.com>
2019-05-07 20:53:28 +0900

    Changed: service

--- provisioning/bin/backup-redmine.sh
+++ provisioning/bin/backup-redmine.sh
@@ -4,7 +4,12 @@ WORK_DIR=`mktemp -d`
 cd ${WORK_DIR}
 
 echo "Dump db data"
-docker exec ${DB_CONTAINER} pg_dump -U ${DB_USERNAME} ${DB_DATABASE} >redmine.`date +%Y%m%d-%H%M%S`.sql
+SQL_FILE=redmine.`date +%Y%m%d-%H%M%S`.sql
+docker exec ${DB_CONTAINER} pg_dump -U ${DB_USERNAME} ${DB_DATABASE} >${SQL_FILE}
+
+echo "Compress db data"
+7z a -mx=9 ${SQL_FILE}.7z ${SQL_FILE}
+rm ${SQL_FILE}
 
 echo "Dump files"
 docker cp ${APP_CONTAINER}:/usr/src/redmine/files/ attachment-file/
--- provisioning/bin/backup-stocks.sh
+++ provisioning/bin/backup-stocks.sh
@@ -1,10 +1,19 @@
-[Unit]
-Description=Backup database
+#!/bin/sh -eu
 
-[Service]
-Type=oneshot
-EnvironmentFile=/etc/profile.d/backup-db.sh
-ExecStart=/usr/local/bin/backup-db.sh
+WORK_DIR=`mktemp -d`
+cd ${WORK_DIR}
 
-[Install]
-WantedBy=multi-user.target
+SQL_FILE=stocks.`date +%Y%m%d-%H%M%S`.sql
+
+echo "Dump db data"
+docker exec ${DB_CONTAINER} pg_dump -U ${DB_USERNAME} ${DB_DATABASE} >${SQL_FILE}
+
+echo "Compress db data"
+7z a -mx=9 ${SQL_FILE}.7z ${SQL_FILE}
+rm ${SQL_FILE}
+
+echo "upload to s3"
+aws --profile ${S3_PROFILE} --endpoint-url ${S3_ENDPOINT} s3 sync . s3://${S3_BUCKET}
+
+echo "cleanup"
+rm -rf ${WORK_DIR}
